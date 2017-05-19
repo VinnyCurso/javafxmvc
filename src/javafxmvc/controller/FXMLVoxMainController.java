8aa5ade69fd64dae1f7c1482f6a02cdea2429cb2
vinnycurso <vinnycaixa@gmail.com>
2017-05-19 17:08:23 -0300

    ...

--- src/javafxmvc/controller/FXMLVoxMainController.java
+++ src/javafxmvc/controller/FXMLVoxMainController.java
@@ -5,9 +5,11 @@
  */
 package javafxmvc.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
@@ -38,5 +40,11 @@ public class FXMLVoxMainController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    public void handleMenuItemCadastrosClientes() throws IOException {
+        
+        AnchorPane a = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafxmvc/view/FXMLCadastroClientes.fxml"));
+        anchorPane.getChildren().setAll(a);
+    }
     
 }
